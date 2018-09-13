ed5f39bd778826379d13eddceaee95917f77e96f
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-13 03:31:03 +0300

    LoveTriangle solution-1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences = []) 
+{
+  var loversCounter=0;
+  
+  for (var i=0; i<preferences.length; i++)
+  {
+    
+    if (preferences[i]== i+1) continue;
+    
+    var first = preferences[i];
+    var second = preferences[first-1];
+    var third = preferences[second-1];
+
+    if (third == i+1)
+      {
+        loversCounter++;
+      }
+  }
+  
+  return loversCounter/3;
+
   // your implementation
 };
